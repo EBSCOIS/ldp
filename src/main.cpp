31b3555523063fb2822cedf666dafee6d59da261
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-08-15 23:04:06 -0400

    Export schema

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include "ldp.h"
+#include "schema.h"
 
 int main(int argc, char* argv[])
 {
-    return main_ldp(argc, argv);
+    // return main_ldp(argc, argv);
+    ldp_schema schema;
+    ldp_schema::make_default_schema(&schema);
+    printf("var defaultFolioSchema = folioSchema{\n");
+    printf("\t[]folioTableSchema{\n");
+    for (auto& table : schema.tables) {
+        printf("\t\tfolioTableSchema{\n");
+        printf("\t\t\tname: \"%s\",\n", table.name.data());
+        printf("\t\t\tmoduleName: \"%s\",\n", table.module_name.data());
+        printf("\t\t\tsourceSpec: \"%s\",\n", table.source_spec.data());
+        printf("\t\t},\n");
+    }
+    printf("\t},\n");
+    printf("}\n");
 }
 
